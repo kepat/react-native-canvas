0d7edbc81be59c0d97da08f1be40c26ab5f3b1ff
Kevin Patrick Tan <kevin@orangekloud.com>
2021-02-18 18:31:15 +0800

    D - Removed the auto scale feature of the canvas based on the device.

--- src/webview.js
+++ src/webview.js
@@ -55,7 +55,7 @@ class AutoScaledCanvas {
 
   set width(value) {
     this.savedWidth = value;
-    this.autoScale();
+    // this.autoScale();
     return value;
   }
 
@@ -65,7 +65,7 @@ class AutoScaledCanvas {
 
   set height(value) {
     this.savedHeight = value;
-    this.autoScale();
+    // this.autoScale();
     return value;
   }
 }
